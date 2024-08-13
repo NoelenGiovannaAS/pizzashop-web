079301f0a88e6909b985f7c357ecb62e41d8ea79
Noelen <noelen_g@outlook.com>
2024-08-12 21:40:34 -0300

    install and config msw and create some mocks

---
+++ src/api/mocks/index.ts
@@ -0,0 +1,13 @@
+import { env } from '@/env'
+import { setupWorker } from 'msw/browser'
+import { signInMock } from './sign-in-mock'
+import { registerRestaurantMock } from './register-restaurant.mock'
+
+export const worker = setupWorker(signInMock, registerRestaurantMock)
+
+export async function enableMSW() {
+  if (env.MODE !== 'test') {
+    return
+  }
+  await worker.start()
+}
